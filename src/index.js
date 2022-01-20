5fedcf7d0c6b87b8fdf2e22f7aaf5bb596a7ee75
realllsiyu <siyuc0712@gmail.com>
2022-01-19 23:42:12 -0500

    Added weather forecast section and rewrote date formatting functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-//display date and time in the fotmat: Monday hh:mm
-function formatDate(date) {
-  var hours = date.getHours();
+//display date and time in the fotmat: date hh:mm
+function formatDate(timestamp) {
+  var date = new Date(timestamp);
 
+  var hours = date.getHours();
   if (hours < 10) {
     hours = "0".concat(hours);
   }
 
   var minutes = date.getMinutes();
-
   if (minutes < 10) {
     minutes = "0".concat(minutes);
   }
 
-  var dayNumber = date.getDay();
   var days = [
     "Sunday",
     "Monday",
@@ -22,33 +21,22 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  var day = days[dayNumber];
-  return "".concat(day, " ").concat(hours, ":").concat(minutes);
+  var day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+//date for forecast
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
 
 var dateElement = document.querySelector("#date");
 var currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
-//display elements in corresponding places
-function displayWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
 //unit conversion
 let unit = "metric";
@@ -75,6 +63,67 @@ function imperialUnit() {
 
 fahrenheit.addEventListener("click", imperialUnit);
 celsius.addEventListener("click", metricUnit);
+
+//weather forecast
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="forecast-temp">
+        <span class="forecast-temp-min"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span><span class="forecast-temp-max"> ${Math.round(
+          forecastDay.temp.max
+        )}° </span></div>
+        </div>
+        `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  document.querySelector("#forecast").innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
+  axios.get(url).then(displayForecast);
+}
+
+//display elements in corresponding places
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
 
 //search for weather at a city
 function getCityWeather(city, unit) {
